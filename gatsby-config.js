8665247f44abf480f8a3e81358969a746e15f4cc
alkharraz <70433617+alkharraz@users.noreply.github.com>
2021-07-13 07:47:56 +0300

    added Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,7 +59,21 @@ module.exports = {
       },
     ],
   },
-  plugins: [
+  plugins: [{
+    resolve: `gatsby-plugin-google-gtag`,
+    options: {
+      // You can add multiple tracking ids and a pageview event will be fired for all of them.
+      trackingIds: [
+        "G-TE2PSWEM5R", // Google Analytics / GA
+      ],
+      // This object is used for configuration specific to this plugin
+      pluginConfig: {
+        // Puts tracking script in the head instead of the body
+        head: true,
+      },
+    },
+  },
+    
     `gatsby-plugin-preload-link-crossorigin`,
     `gatsby-plugin-catch-links`,
     {
